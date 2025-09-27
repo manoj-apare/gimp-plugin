b044aa7b679fda1b439ab318609265cbeb8069d9
Manoj K <manojapare@gmail.com>
2025-09-27 16:13:27 +0530

    Add my-frame plugin

---
+++ my-frame/my-frame.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import gi
+gi.require_version('Gimp', '3.0')
+from gi.repository import Gimp
+gi.require_version('GimpUi', '3.0')
+from gi.repository import GimpUi
+gi.require_version('Gegl', '0.4')
+from gi.repository import Gegl
+from gi.repository import GObject
+from gi.repository import GLib
+import sys
+
+def N_(message): return message
+def _(message): return GLib.dgettext(None, message)
+
+'''
+A Python plugin.
+My Frame.
+'''
+
+def add_frame(procedure, run_mode, image, drawables, config, data):
+    if run_mode == Gimp.RunMode.INTERACTIVE:
+        GimpUi.init('python-fu-my-frame')
+        Gegl.init(None)
+        dialog = GimpUi.ProcedureDialog(procedure=procedure, config=config)
+        dialog.fill(None)
+        if not dialog.run():
+            dialog.destroy()
+            return procedure.new_return_values(Gimp.PDBStatusType.CANCEL, GLib.Error())
+        else:
+            dialog.destroy()
+    opacity = config.get_property('opacity')
+
+    if opacity is None:
+        error = 'No opacity given'
+        return procedure.new_return_values(Gimp.PDBStatusType.CALLING_ERROR, GLib.Error(error))
+    
+    image.undo_group_start()
+    
+    # 1. Find image resolution
+    imageWidth = image.get_width()
+    imageHeight = image.get_height()
+    
+    # 2. Find smallest side and calculate border thickness (0.05%)
+    small_side = min(imageWidth, imageHeight)
+    border_thickness = int(0.05 * small_side)
+
+    # 3. Resize image canvas to add border space
+    borderWidth = imageWidth + 2*border_thickness
+    borderHeight = imageHeight + 2*border_thickness
+    image.resize(borderWidth, borderHeight, border_thickness, border_thickness)
+    
+    # 4. Duplicate the image layer (at bottom)
+    duplicate_layer = drawables[0].copy()
+    image.insert_layer(duplicate_layer, None, 2)
+    
+    # 5. Add white border layer (in middle)
+    border_layer = Gimp.Layer.new(image, "Border",
+                                 borderWidth,
+                                 borderHeight,
+                                 Gimp.ImageType.RGB_IMAGE,
+                                 100,
+                                 Gimp.LayerMode.NORMAL)
+    
+    image.insert_layer(border_layer, None, 1)
+    border_layer.fill(Gimp.FillType.WHITE)
+    
+    # 6. Scale the duplicate layer up by 20%
+    new_width = int(imageWidth * 1.2)
+    new_height = int(imageHeight * 1.2)
+    duplicate_layer.scale(new_width, new_height, False)
+    
+    # 7. Center the scaled duplicate layer
+    offset_x = (borderWidth - new_width) // 2
+    offset_y = (borderHeight - new_height) // 2
+    duplicate_layer.set_offsets(offset_x, offset_y)
+    
+    # 8. Set border layer opacity
+    border_layer.set_opacity(opacity)
+
+    image.undo_group_end()
+    Gimp.displays_flush()
+
+    return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
+
+class MyFramePlugin (Gimp.PlugIn):
+    # FUTURE all other Gimp classes that have GimpParamSpecs
+
+    ## GimpPlugIn virtual methods ##
+    def do_set_i18n(self, procname):
+        return False
+
+    def do_query_procedures(self):
+        return [ 'python-fu-my-frame' ]
+
+    def do_create_procedure(self, name):
+        procedure = Gimp.ImageProcedure.new(self, name, Gimp.PDBProcType.PLUGIN, add_frame, None)
+        procedure.set_image_types("*")
+        procedure.set_documentation ("My Frame ", "My Frame ", name)
+        procedure.set_menu_label("My Frame")
+        procedure.set_attribution("Manoj Kumar", "Manoj Kumar", "2025")
+        # Top level menu "Test"
+        procedure.add_menu_path ("<Image>/Filters/Frames/")
+        # # Get frame opacity (default 60).
+        procedure.add_int_argument ("opacity", "_Opacity", "Opacity", 1, 100, 60, GObject.ParamFlags.READWRITE)
+
+        return procedure
+
+Gimp.main(MyFramePlugin.__gtype__, sys.argv)
